ce301cd0fb2e3fe6520e39913a75f5731b2a9983
Malin W. <50824014+malinwaage@users.noreply.github.com>
2025-04-28 14:32:11 +0200

    Update constants.py

--- config/constants.py
+++ config/constants.py
@@ -1,7 +1,8 @@
 import os
 
 # Constants
-API_KEY = os.environ.get('NVE_API_KEY', 'your_api_key_here')
+#API_KEY = os.environ.get('NVE_API_KEY', 'your_api_key_here')
+API_KEY = os.environ.get('3G2Xx1Sw8kGpYdbzdskSew==')
 BASE_URL = 'https://hydapi.nve.no/api/v1/Observations'
 SEQUENCE_LENGTH = 24
 FORECAST_HORIZON = 24
